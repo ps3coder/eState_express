1da83da119df73083de5ef465e249cdbecb118e7
ps3coder <ss559679035@gmail.com>
2024-11-02 17:10:47 +0530

    first update

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+
+const app = express();
+const port = 8800;
+// app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // console.log(hashedPassword);
+  // create a new user and save to DB
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  console.log(newUser);
+};
+export const login = (req, res) => {
+  // db operations
+  console.log("login endpoints");
+};
+export const logout = (req, res) => {
+  // db operations
+  console.log("logout endpoints");
+};
