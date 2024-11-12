2713df6c332f1338a10ebb2f7efd6eaef82ece58
ps3coder <ss559679035@gmail.com>
2024-11-12 18:29:40 +0530

    1211

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 const app = express();
 const corsOrigin = {
   origin: process.env.CLIENT_URL,
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
